c6bf6bca0d0e50588b7ec3f34fec2cab2e5c2088
Gaurav Pancholi <gaurav.pancholi2@gmail.com>
2021-02-27 17:44:57 +0530

    Added Service , controller and View

---
+++ src/main/java/guru/springframework/JokesApp/controller/JokeController.java
@@ -0,0 +1,25 @@
+package guru.springframework.JokesApp.controller;
+
+import guru.springframework.JokesApp.services.JokesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class JokeController {
+
+    private final JokesService jokesService;
+
+    public JokeController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model)
+    {
+        model.addAttribute("joke",jokesService.getJoke());
+        return "index";
+
+    }
+}
